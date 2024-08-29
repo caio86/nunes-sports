2eadd79b4375a6677951278442c46f889e6dd736
caio86 <caioluiz86@gmail.com>
2024-08-29 00:43:08 -0300

    feat: implement better logic to GetProduct

--- backend/internal/adapters/input/api/handlers/handlers_test.go
+++ backend/internal/adapters/input/api/handlers/handlers_test.go
@@ -11,6 +11,21 @@ import (
 )
 
 func TestGETProducts(t *testing.T) {
+	t.Run("get product with id 1", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/api/v1/products/1", nil)
+		response := httptest.NewRecorder()
+
+		GetProduct(response, request)
+
+		got := response.Body.String()
+		want := new(strings.Builder)
+		json.NewEncoder(want).Encode(domain.Product{ID: 1})
+
+		if got != want.String() {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+
 	t.Run("get product with id 2", func(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodGet, "/api/v1/products/2", nil)
 		response := httptest.NewRecorder()
--- backend/internal/adapters/input/api/handlers/product_handler.go
+++ backend/internal/adapters/input/api/handlers/product_handler.go
@@ -3,16 +3,23 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/caio86/nunes-sports/backend/internal/core/domain"
 )
 
 func GetProduct(w http.ResponseWriter, r *http.Request) {
-	data := domain.Product{ID: 2}
+	id, _ := strconv.Atoi(strings.TrimPrefix(r.URL.Path, "/api/v1/products/"))
+	data := domain.Product{ID: id}
 
+	renderJSON(w, http.StatusOK, data)
+}
+
+func renderJSON(w http.ResponseWriter, statusCode int, v interface{}) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	err := json.NewEncoder(w).Encode(data)
+	w.WriteHeader(statusCode)
+	err := json.NewEncoder(w).Encode(v)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return

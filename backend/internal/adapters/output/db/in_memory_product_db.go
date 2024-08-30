5475cf8bc193ce9cff0b16be2d3f7fe2491459f9
caio86 <caioluiz86@gmail.com>
2024-08-30 16:01:35 -0300

    feat: implement CreateProduct on InMemoryProductDB

--- backend/internal/adapters/output/db/in_memory_product_db.go
+++ backend/internal/adapters/output/db/in_memory_product_db.go
@@ -1,13 +1,14 @@
 package db
 
 import (
-	"fmt"
+	"errors"
 
 	"github.com/caio86/nunes-sports/backend/internal/core/domain"
 )
 
 type InMemoryProductDB struct {
-	store map[int]*domain.Product
+	store  map[int]*domain.Product
+	lastID int
 }
 
 func (i *InMemoryProductDB) FindAllProducts() []*domain.Product {
@@ -21,7 +22,20 @@ func (i *InMemoryProductDB) FindAllProducts() []*domain.Product {
 func (i *InMemoryProductDB) FindProductByID(id int) (*domain.Product, error) {
 	product, ok := i.store[id]
 	if !ok {
-		return nil, fmt.Errorf("product with id %d not found", id)
+		return nil, errors.New("could not find the requested product")
 	}
 	return product, nil
+}
+
+func (i *InMemoryProductDB) CreateProduct(product *domain.Product) error {
+	i.lastID++
+	product.ID = i.lastID
+
+	_, err := i.FindProductByID(product.ID)
+	if err == nil {
+		return errors.New("product already exists")
+	}
+
+	i.store[product.ID] = product
+	return nil
 }

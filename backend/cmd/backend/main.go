a0280dadb02f6c2663cd6e82a2f37318e23937dd
caio86 <caioluiz86@gmail.com>
2024-09-02 23:03:16 -0300

    feat: enable CORS

--- backend/cmd/backend/main.go
+++ backend/cmd/backend/main.go
@@ -9,10 +9,26 @@ import (
 	"github.com/caio86/nunes-sports/backend/internal/core/service"
 )
 
+func CORS(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
+		if r.Method == "OPTIONS" {
+			http.Error(w, "No Content", http.StatusNoContent)
+			return
+		}
+
+		next(w, r)
+	}
+}
+
 func main() {
 	store := db.NewInMemoryProductDB()
 	service := service.NewProductServiceImpl(store)
 	handler := handlers.NewProductHandler(service)
 
-	log.Fatal(http.ListenAndServe(":5000", handler))
+	log.Fatal(http.ListenAndServe(":5000", CORS(handler.ServeHTTP)))
 }

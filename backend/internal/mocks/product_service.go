a688bddbd98bb24c6002474e2e03b838be055317
caio86 <caioluiz86@gmail.com>
2024-09-30 17:58:22 -0300

    feat(mock): create product service mock

---
+++ backend/internal/mocks/product_service.go
@@ -0,0 +1,29 @@
+package mocks
+
+import (
+	"github.com/caio86/nunes-sports/backend/internal/core/domain"
+	"github.com/stretchr/testify/mock"
+)
+
+type productService struct {
+	mock.Mock
+}
+
+func NewProductService() *productService {
+	return &productService{}
+}
+
+func (m *productService) GetProducts(page_index, page_size int) ([]*domain.Product, int64, error) {
+	args := m.Called(page_index, page_size)
+	return args.Get(0).([]*domain.Product), args.Get(1).(int64), args.Error(2)
+}
+
+func (m *productService) GetProductByID(id string) (*domain.Product, error) {
+	args := m.Called(id)
+	return args.Get(0).(*domain.Product), args.Error(1)
+}
+
+func (m *productService) CreateProduct(product *domain.Product) (*domain.Product, error) {
+	args := m.Called(product)
+	return args.Get(0).(*domain.Product), args.Error(1)
+}

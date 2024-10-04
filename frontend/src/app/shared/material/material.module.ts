a0b4279b25b2dbb35fb695614d5e75487caee386
caio86 <caioluiz86@gmail.com>
2024-10-04 20:53:45 -0300

    feat(frontend): add Angular Material module and integrate into AppModule
    - Created `MaterialModule` in `shared/material/material.module.ts` to consolidate Angular Material components.
    - Imported `HttpClientModule` and `MaterialModule` into `AppModule` for application-wide availability.
    - This setup enables the use of Angular Material components throughout the application.

---
+++ frontend/src/app/shared/material/material.module.ts
@@ -0,0 +1,94 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from "@angular/material/button"
+import { MatCheckboxModule } from "@angular/material/checkbox"
+import { MatSidenavModule } from "@angular/material/sidenav"
+import { MatListModule } from "@angular/material/list"
+import { MatTabsModule } from "@angular/material/tabs"
+import { MatCardModule } from "@angular/material/card"
+import { MatExpansionModule } from "@angular/material/expansion"
+import { MatRadioModule } from "@angular/material/radio"
+import { MatInputModule } from "@angular/material/input"
+import { MatSnackBarModule } from "@angular/material/snack-bar"
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner"
+import { MatBottomSheetModule } from "@angular/material/bottom-sheet"
+import { MatBadgeModule } from "@angular/material/badge"
+import { MatGridListModule } from "@angular/material/grid-list"
+import { MatSelectModule } from "@angular/material/select"
+import { MatChipsModule } from "@angular/material/chips"
+import { MatSlideToggleModule } from "@angular/material/slide-toggle"
+import { MatMenuModule } from "@angular/material/menu"
+import { MatTooltipModule } from "@angular/material/tooltip"
+import { MatToolbarModule } from "@angular/material/toolbar"
+import { MatTableModule } from "@angular/material/table"
+import { MatPaginatorModule } from "@angular/material/paginator"
+import { MatSortModule } from "@angular/material/sort"
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatDatepickerModule } from "@angular/material/datepicker"
+import { MatNativeDateModule } from '@angular/material/core';
+
+
+
+@NgModule({
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTabsModule,
+    MatCardModule,
+    MatListModule,
+    MatExpansionModule,
+    MatRadioModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatBottomSheetModule,
+    MatBadgeModule,
+    MatGridListModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatTooltipModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
+  exports: [
+    MatIconModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTabsModule,
+    MatCardModule,
+    MatListModule,
+    MatExpansionModule,
+    MatRadioModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatBottomSheetModule,
+    MatBadgeModule,
+    MatGridListModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatTooltipModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
+})
+export class MaterialModule { }

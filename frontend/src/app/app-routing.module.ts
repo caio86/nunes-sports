aaa95873b8e94d3d2ab9ede414d6e3c9c864cd04
caio86 <caioluiz86@gmail.com>
2024-10-05 19:58:45 -0300

    feat: add listagem-produto to router

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListagemProdutoComponent } from './pages/produto/listagem-produto/listagem-produto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "produto",
+    children: [
+      {
+        path: "",
+        component: ListagemProdutoComponent,
+      },
+    ],
+  },
+  {
+    path: "",
+    component: ListagemProdutoComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
